f8a42337c193c8cd0ae02da6435872f612e635a4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using DevExpress.Xpf.RichEdit;
+
+namespace RichEditTwoControlsOneToolbarWpf {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            richEditControl1.KeyCodeConverter.Focus();
+        }
+
+        private void richEditControl_GotFocus(object sender, RoutedEventArgs e) {
+            RichEditControl richEditControl = (RichEditControl)sender;
+
+            richEditControl1.BarManager = null;
+            richEditControl2.BarManager = null;
+            richEditControl.BarManager = barManager1;
+            richEditControlProvider1.RichEditControl = richEditControl;
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,25 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.RichEdit
+
+Namespace RichEditTwoControlsOneToolbarWpf
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richEditControl1.KeyCodeConverter.Focus()
+		End Sub
+
+		Private Sub richEditControl_GotFocus(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim richEditControl As RichEditControl = CType(sender, RichEditControl)
+
+			richEditControl1.BarManager = Nothing
+			richEditControl2.BarManager = Nothing
+			richEditControl.BarManager = barManager1
+			richEditControlProvider1.RichEditControl = richEditControl
+		End Sub
+	End Class
+End Namespace
